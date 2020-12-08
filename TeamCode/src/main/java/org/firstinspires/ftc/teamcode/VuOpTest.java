4db85bd0472d098521e07a404fde62e003752f43
afisher2 <afisher1@gmail.com>
2020-12-07 20:16:41 -0600

    SLIDE FIX

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuOpTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class VuOpTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        UltimateVuforia Vu = new UltimateVuforia(this);
+        Vu.yesVuforia();
+        waitForStart();
+
+        while (opModeIsActive()){
+        CoordHolder coord2= Vu.getCoords();
+        if (coord2.seeImage) {
+            telemetry.addData("x", coord2.x);
+            telemetry.addData("y", coord2.y);
+            telemetry.addData("angle", coord2.angle);
+        } else {
+            telemetry.addData("noImage", "");
+        }
+        telemetry.update();
+        }
+    }
+}
