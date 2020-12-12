41d96161803db64ff53ba1cf9f0e12e2f8fd45a9
Clarekpd <Clarekpd@outlook.com>
2020-12-12 10:59:43 -0600

    Vufoira changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuNavTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuNavTest.java
@@ -24,7 +24,7 @@ public class VuNavTest extends LinearOpMode {
         // go forward 115 inches
         bot.goForward(1,115);
         // Ask Vuforia if we are at (43, 35) heading is 0 degrees; if an image is seen then:
-        vu.getCoords();
+        cH = vu.getCoords();
         if(cH.seeImage) {
 
             // If heading is not zero, then turn robot to correct
@@ -32,23 +32,23 @@ public class VuNavTest extends LinearOpMode {
                 bot.turnRight(cH.angle, .25);
                 //assuming cH is based on angle right
             } else if (cH.angle < 0 ) {
-                bot.turnLeft(cH.angle, .25);
+                bot.turnLeft(-cH.angle, .25);
                 //assuming cH is based on angle right
             }
 
             // If X < 43 then go farward the difference; if X > 43 then backup the difference
             if (cH.x < 43) {
-                bot.goForward(1, (43 - cH.x));
+                bot.goForward(.5, (43 - cH.x));
                 //assuming slide left is positive
             } else if (cH.x > 43) {
-                bot.goForward(-1, (cH.x - 43));
+                bot.goForward(-.5, (cH.x - 43));
                 //assuming slide left is positive
             }
             // If Y < 35 then go slide left the difference; if Y > 35 then slide right the difference
             if (cH.y < 35) {
-                bot.slide(-1, (35 - cH.y));
+                bot.slide(-.5, (35 - cH.y));
             } else if (cH.y > 35) {
-                bot.slide(1, (cH.y - 35));
+                bot.slide(.5, (cH.y - 35));
             }
         }
         // If image not seen do nothing
