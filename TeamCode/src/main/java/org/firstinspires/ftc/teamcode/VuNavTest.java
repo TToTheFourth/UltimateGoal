0a79e777abe9167abfd080a28aa57a0bc5b899ad
Andy Fisher <afisher2@users.noreply.github.com>
2020-12-11 19:06:51 -0600

    Vuforia navigation test - changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuNavTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuNavTest.java
@@ -23,15 +23,20 @@ public class VuNavTest extends LinearOpMode {
         // Navigate from (-72, 35) to (43, 35)
         // go forward 115 inches
         bot.goForward(1,115);
-        //TODO: ask Vuforia if we are at (43, 35) heading is 0 degrees; if an image is seen then:
+        // Ask Vuforia if we are at (43, 35) heading is 0 degrees; if an image is seen then:
+        // TODO: need to call vu.getCoords() here - this returns the CoordHolder to test in the following If statement
         if(cH.seeImage) {
+            // TODO: move this before the if statement
             vu.getCoords();
-            // TODO:     if heading is not zero, then turn robot to correct
+
+            // If heading is not zero, then turn robot to correct
+            // TODO: if the angle is positive, turn right; if it is negative, turn left
             if (cH.angle != 0) {
                 bot.turnLeft(cH.angle, .25);
                 //assuming cH is based on angle right
             }
-            // TODO:     if X < 43 then go farward the difference; if X > 43 then backup the difference
+
+            // If X < 43 then go farward the difference; if X > 43 then backup the difference
             if (cH.x < 43) {
                 bot.goForward(1, (43 - cH.x));
                 //assuming slide left is positive
@@ -39,15 +44,16 @@ public class VuNavTest extends LinearOpMode {
                 bot.goForward(-1, (cH.x - 43));
                 //assuming slide left is positive
             }
-            // TODO:     if Y < 35 then go slide left the difference; if Y > 35 then slide right the difference
+            // If Y < 35 then go slide left the difference; if Y > 35 then slide right the difference
             if (cH.y < 35) {
                 bot.slide(-1, (35 - cH.y));
             } else if (cH.y > 35) {
                 bot.slide(1, (cH.y - 35));
             }
         }
-        // TODO: if image not seen do nothing
-        // TODO: turn off Vuforia
+        // If image not seen do nothing
+
+        // Turn off Vuforia
         Vuforia.deinit();
     }
 }
