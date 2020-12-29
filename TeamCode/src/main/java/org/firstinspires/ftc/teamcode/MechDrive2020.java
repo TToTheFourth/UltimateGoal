b91f0fff9bffac6a77c3a463d65c8019fa2b9aea
Clarekpd <Clarekpd@outlook.com>
2020-12-28 19:38:21 -0600

    MechDrive2020 updated with the convoy code, ready to test.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechDrive2020.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechDrive2020.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 
 
 @TeleOp
@@ -17,7 +16,15 @@ public class MechDrive2020 extends LinearOpMode {
     // TODO: declare motor for claw and elbow
     private DcMotor claw;
     private  DcMotor elbow;
+    private DcMotor convoy;
 
+    static final double INCREMENT   = 0.01;
+    static final int    CYCLE_MS    =   50;
+    static final double MAX_FWD     =  1.0;
+    static final double MAX_REV     = -1.0;
+
+    double  power   = 0;
+    boolean rampUp  = true;
 
     @Override
     public void runOpMode() {
@@ -30,6 +37,7 @@ public class MechDrive2020 extends LinearOpMode {
         // TODO: get claw and elbow motors
         claw = hardwareMap.get(DcMotor.class, "claw0");
         elbow = hardwareMap.get(DcMotor.class, "elbow1");
+        convoy = hardwareMap.get(DcMotor.class, "convey2");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -40,6 +48,7 @@ public class MechDrive2020 extends LinearOpMode {
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        convoy.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         // makes sure the robot doesn't drift
 
         while (opModeIsActive()) {
@@ -48,10 +57,6 @@ public class MechDrive2020 extends LinearOpMode {
             double rightY_G1;
             double leftX_G1;
             double leftY_G1;
-            double leftX_G2;
-            double leftY_G2;
-            double rightX_G2;
-            double rightY_G2;
 
             rightY_G1 = -gamepad1.right_stick_y;
             rightX_G1 = -gamepad1.right_stick_x;
@@ -62,6 +67,53 @@ public class MechDrive2020 extends LinearOpMode {
             double backLeft = (rightX_G1 + rightY_G1 + leftX_G1);
             double backRight = (rightX_G1 - rightY_G1 + leftX_G1);
             double frontRight = (rightX_G1 - rightY_G1 - leftX_G1);
+
+            if (rampUp) {
+                // Keep stepping up until we hit the max value.
+                frontLeft += INCREMENT;
+                frontRight += INCREMENT;
+                backLeft += INCREMENT;
+                backRight += INCREMENT;
+                if (frontLeft >= MAX_FWD ) {
+                    frontLeft = MAX_FWD;
+                    rampUp = !rampUp;   // Switch ramp direction
+                }
+                if (frontRight >= MAX_FWD ) {
+                    frontRight = MAX_FWD;
+                    rampUp = !rampUp;   // Switch ramp direction
+                }
+                if (backLeft >= MAX_FWD ) {
+                    backLeft = MAX_FWD;
+                    rampUp = !rampUp;   // Switch ramp direction
+                }
+                if (backRight >= MAX_FWD ) {
+                    backRight = MAX_FWD;
+                    rampUp = !rampUp;   // Switch ramp direction
+                }
+            }
+            else {
+                // Keep stepping down until we hit the min value.
+                frontLeft -= INCREMENT ;
+                frontRight -= INCREMENT ;
+                backLeft -= INCREMENT ;
+                backRight -= INCREMENT ;
+                if (frontLeft <= MAX_REV ) {
+                    frontLeft = MAX_REV;
+                    rampUp = !rampUp;  // Switch ramp direction
+                }
+                if (frontRight <= MAX_REV ) {
+                    frontRight = MAX_REV;
+                    rampUp = !rampUp;  // Switch ramp direction
+                }
+                if (backLeft <= MAX_REV ) {
+                    backLeft = MAX_REV;
+                    rampUp = !rampUp;  // Switch ramp direction
+                }
+                if (backRight <= MAX_REV ) {
+                    backRight = MAX_REV;
+                    rampUp = !rampUp;  // Switch ramp direction
+                }
+            }
 
             frontLeftMotor.setPower(frontLeft);
             backLeftMotor.setPower(backLeft);
@@ -86,12 +138,24 @@ public class MechDrive2020 extends LinearOpMode {
                 elbow.setPower(0);
             }
 
+            if (gamepad1.a){
+                convoy.setPower(1);
+            } else if (gamepad1.y){
+                convoy.setPower(-1);
+            } else {
+                convoy.setPower(0);
+            }
+
+
         }
 
         frontLeftMotor.setPower(0);
         backLeftMotor.setPower(0);
         frontRightMotor.setPower(0);
         backRightMotor.setPower(0);
+        convoy.setPower(0);
+        elbow.setPower(0);
+        claw.setPower(0);
         // turns off motors
     }
 }
