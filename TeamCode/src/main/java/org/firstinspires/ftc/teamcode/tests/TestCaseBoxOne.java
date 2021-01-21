9605a2f1b1b9639cace3be1e0ce040ae871f09d0
Clarekpd <Clarekpd@outlook.com>
2021-01-20 19:28:49 -0600

    TestCaseBox classes, ready for test and in the test folder

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TestCaseBoxOne.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.CoordHolder;
+import org.firstinspires.ftc.teamcode.RepresentoBotMVP;
+import org.firstinspires.ftc.teamcode.UltimateVuforia;
+import org.firstinspires.ftc.teamcode.VuforiaNavigator;
+
+@Autonomous
+public class TestCaseBoxOne extends LinearOpMode {
+    RepresentoBotMVP bot;
+//    UltimateVuforia vu;
+//    int rings;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        bot=new RepresentoBotMVP(this);
+//        vu = new UltimateVuforia(this);
+        waitForStart();
+        bot.startGyro();
+//        vu.yesVuforia();
+
+//        rings = vu.tensorflow();
+//        telemetry.addData("rings", rings);
+//        telemetry.update();
+//        if (rings == 1) {
+            bot.goForward(0.5, 5);
+            bot.turnLeft(90, 0.3);
+            bot.goForward(0.5, 19);
+            bot.turnRight(90, 0.3);
+            bot.goForward(0.5, 55);
+//        }
+//        vu.noVuforia();
+    }
+}
