79449cbd6126a6e2cd4225ccedaff04d3b34ae74
Clarekpd <Clarekpd@outlook.com>
2021-01-20 19:27:43 -0600

    TestCaseBox classes, ready for test and in the test folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TestCaseBoxZero.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TestCaseBoxZero.java
@@ -1,37 +1,39 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.vuforia.Vuforia;
 
+import org.firstinspires.ftc.teamcode.CoordHolder;
+import org.firstinspires.ftc.teamcode.RepresentoBotMVP;
+import org.firstinspires.ftc.teamcode.UltimateVuforia;
+import org.firstinspires.ftc.teamcode.VuforiaNavigator;
 import org.tensorflow.lite.TensorFlowLite;
 
 @Autonomous
 public class TestCaseBoxZero extends LinearOpMode {
     RepresentoBotMVP bot;
-    CoordHolder cH;
-    VuforiaNavigator vuNav;
-    UltimateVuforia vu;
-    int rings;
+//    UltimateVuforia vu;
+//    int rings;
 
     @Override
     public void runOpMode() throws InterruptedException {
         bot=new RepresentoBotMVP(this);
-        vu = new UltimateVuforia(this);
+//        vu = new UltimateVuforia(this);
         waitForStart();
         bot.startGyro();
-        vu.yesVuforia();
+//        vu.yesVuforia();
 
-        rings = vu.tensorflow();
-        telemetry.addData("rings", rings);
-        telemetry.update();
-        if (rings == 0) {
+//        rings = vu.tensorflow();
+//        telemetry.addData("rings", rings);
+//        telemetry.update();
+//        if (rings == 0) {
             bot.goForward(0.5, 5);
             bot.turnLeft(90, 0.3);
             bot.goForward(0.5, 19);
             bot.turnRight(90, 0.3);
             bot.goForward(0.5, 55);
-        }
-        vu.noVuforia();
+//        }
+//        vu.noVuforia();
     }
 }
