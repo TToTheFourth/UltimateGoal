a06c8410faca198e3c09dd0cca4c95b23347566f
Clarekpd <Clarekpd@outlook.com>
2021-01-25 19:37:34 -0600

    changes, 1/25

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TestCaseWobbleGoal.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.RepresentoBotMVP;
+
+@Autonomous
+public class TestCaseWobbleGoal extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RepresentoBotMVP bot = new RepresentoBotMVP(this);
+        bot.startGyro();
+        waitForStart();
+        bot.clawClosePosition();
+        sleep(1000);
+        bot.clawOpenPosition();
+    }
+}
