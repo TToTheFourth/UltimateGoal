e6d6d2bdc73980602e8f8aba39a4515e7e79dc52
Andy Fisher <afisher2@users.noreply.github.com>
2021-02-17 19:06:46 -0600

    gyro correction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechDrive2020.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechDrive2020.java
@@ -14,11 +14,10 @@ public class MechDrive2020 extends LinearOpMode {
     private DcMotor frontLeftMotor;
     private DcMotor backRightMotor;
     private DcMotor frontRightMotor;
-    RepresentoBotMVP bot;
-    UltimateVuforia vu;
-    VuforiaNavigator vuNav;
+//    RepresentoBotMVP bot;
+//    UltimateVuforia vu;
+//    VuforiaNavigator vuNav;
 
-    // TODO: declare motor for claw and elbow
     private DcMotor thrower;
     private DcMotor convoy;
     private Servo claw;
@@ -38,19 +37,16 @@ public class MechDrive2020 extends LinearOpMode {
     @Override
     public void runOpMode() {
 
-        bot=new RepresentoBotMVP(this);
-        vu = new UltimateVuforia(this);
-        vuNav = new VuforiaNavigator(this, bot, vu);
-        waitForStart();
-        bot.startGyro();
-        Vuforia.init();
+        //bot=new RepresentoBotMVP(this);
+        //vu = new UltimateVuforia(this);
+        //vuNav = new VuforiaNavigator(this, bot, vu);
+        //bot.startGyro();
+        //Vuforia.init();
 
         backLeftMotor = hardwareMap.get(DcMotor.class, "motor0");
         frontLeftMotor = hardwareMap.get(DcMotor.class, "motor1");
         frontRightMotor = hardwareMap.get(DcMotor.class, "motor2");
         backRightMotor = hardwareMap.get(DcMotor.class, "motor3");
-
-        // TODO: get claw and elbow motors
 
         thrower = hardwareMap.get(DcMotor.class, "thrower");
         convoy = hardwareMap.get(DcMotor.class, "convey2");
@@ -59,17 +55,17 @@ public class MechDrive2020 extends LinearOpMode {
         miniSweep = hardwareMap.get(Servo.class, "servoSweep");
         sweeper = hardwareMap.get(DcMotor.class, "sweeper");
 
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        waitForStart();
-
         frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         sweeper.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         // makes sure the robot doesn't drift
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
 
         while (opModeIsActive()) {
 
@@ -83,108 +79,26 @@ public class MechDrive2020 extends LinearOpMode {
             leftY_G1 = gamepad1.left_stick_y;
             leftX_G1 = -gamepad1.left_stick_x;
 
+            if (gamepad1.x) {
+                speed = 0.5;
+            } else if (gamepad1.y) {
+                speed = 1.0;
+            }
+
             double frontLeft = (rightX_G1 + rightY_G1 - leftX_G1) * (speed);
             double backLeft = (rightX_G1 + rightY_G1 + leftX_G1) * (speed);
             double backRight = (rightX_G1 - rightY_G1 + leftX_G1) * (speed);
             double frontRight = (rightX_G1 - rightY_G1 - leftX_G1) * (speed);
 
-            telemetry.addData("frontLeft %.02f", frontLeft);
-            telemetry.addData("frontRight %.02f", frontRight);
-            telemetry.addData("backLeft %.02f", backLeft);
-            telemetry.addData("backRight %.02f", backRight);
-/*
-            if (rampUp) {
-                // Keep stepping up until we hit the max value.
-                frontLeft += INCREMENT;
-                frontRight += INCREMENT;
-                backLeft += INCREMENT;
-                backRight += INCREMENT;
-                if (frontLeft >= MAX_FWD ) {
-                    frontLeft = MAX_FWD;
-                    rampUp = !rampUp;   // Switch ramp direction
-                }
-                if (frontRight >= MAX_FWD ) {
-                    frontRight = MAX_FWD;
-                    rampUp = !rampUp;   // Switch ramp direction
-                }
-                if (backLeft >= MAX_FWD ) {
-                    backLeft = MAX_FWD;
-                    rampUp = !rampUp;   // Switch ramp direction
-                }
-                if (backRight >= MAX_FWD ) {
-                    backRight = MAX_FWD;
-                    rampUp = !rampUp;   // Switch ramp direction
-                }
-            }
-            else {
-                // Keep stepping down until we hit the min value.
-                frontLeft -= INCREMENT ;
-                frontRight -= INCREMENT ;
-                backLeft -= INCREMENT ;
-                backRight -= INCREMENT ;
-                if (frontLeft <= MAX_REV ) {
-                    frontLeft = MAX_REV;
-                    rampUp = !rampUp;  // Switch ramp direction
-                }
-                if (frontRight <= MAX_REV ) {
-                    frontRight = MAX_REV;
-                    rampUp = !rampUp;  // Switch ramp direction
-                }
-                if (backLeft <= MAX_REV ) {
-                    backLeft = MAX_REV;
-                    rampUp = !rampUp;  // Switch ramp direction
-                }
-                if (backRight <= MAX_REV ) {
-                    backRight = MAX_REV;
-                    rampUp = !rampUp;  // Switch ramp direction
-                }
-            }
-*/
+
             frontLeftMotor.setPower(frontLeft);
             backLeftMotor.setPower(backLeft);
             backRightMotor.setPower(backRight);
             frontRightMotor.setPower(frontRight);
 
-//            if(gamepad1.a) {
-//                vu.getCoords();
-//                vuNav.navigate(-6,36, 0);
-//                if(gamepad1.b) {
-//                    break;
-//                }
-//            }
-//
-//            if (gamepad1.x){
-//                vu.getCoords();
-//                telemetry.addData("coords", vu.getCoords());
-//            }
 
-            if (gamepad1.left_bumper) {
-                speed = 0.5;
-            } else if (gamepad1.x) {
-                speed = 0.25;
-            } else if (gamepad1.y) {
-                speed = 1;
-            }
             //Todo: add choord for power shot
 
-            // TODO: get input from controller dpad left - right for claw
-            //if (gamepad2.dpad_left){
-            //    claw.setPower(0.5);
-            //} else if (gamepad2.x){
-            ///    claw.setPower(-0.5);
-            //} else {
-            //    claw.setPower(0);
-            //}
-
-//            if(gamepad2.right_bumper) {
-//                // thrower on
-//                thrower.setPower(1);
-//            } else {
-//                // thrower off
-//                thrower.setPower(0);
-//            }
-
-            // TODO: get input from controller dpad up - down for elbow
             if (gamepad2.left_bumper || gamepad2.dpad_up) {
                 thrower.setPower(1);
             } else {
@@ -198,25 +112,6 @@ public class MechDrive2020 extends LinearOpMode {
             } else {
                 convoy.setPower(0);
             }
-
-            //if (gamepad2.left_bumper) {
-            //    thrower.setPower(1);
-            //} else if (gamepad2.dpad_down) {
-            //    convoy.setPower(0.5);
-            //    thrower.setPower(0);
-            //} else if (gamepad2.dpad_up){
-            //    thrower.setPower(1);
-            //    convoy.setPower(-0.5);
-            //} else {
-            //    convoy.setPower(0);
-            //    thrower.setPower(0);
-            //}
-
-//            if (gamepad2.right_bumper) {
-//                claw.setPosition(0.5);
-//            } else {
-//                claw.setPosition(0.3);
-//            }
 
             if (gamepad2.right_trigger > 0) {
                 elbow.setPower(0.5);
@@ -246,7 +141,6 @@ public class MechDrive2020 extends LinearOpMode {
                 sweeper.setPower(0);
             }
 
-            telemetry.update();
         }
 
         frontLeftMotor.setPower(0);
@@ -256,7 +150,6 @@ public class MechDrive2020 extends LinearOpMode {
         convoy.setPower(0);
         thrower.setPower(0);
         elbow.setPower(0);
-        Vuforia.deinit();
         // turns off motors
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RepresentoBotMVP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RepresentoBotMVP.java
@@ -562,6 +562,53 @@ public class RepresentoBotMVP {
         // sets motors to zero
     }
 
+    public void goForwardGyroErrorCorrection(double power, double distance){
+        frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // sets the encoders
+
+        double rightY_G1 = 1.0 * power;
+        double rightX_G1 = 0.0;
+        double leftX_G1 = 0.0;
+        // sets power
+
+        // sets the correct variables to the motors
+
+        long ticks = ticksToInchesForward(distance);
+        miniGyro.reset();
+        while (opMode.opModeIsActive()) {
+            int rotations = frontRightMotor.getCurrentPosition();
+            if (rotations<0) {
+                rotations = rotations * -1;
+            }
+            if (rotations >= ticks) {
+                break;
+            }
+
+            // Get the current heading; anything other than 0 is off course
+            // this will return positive angle if drifting CCW
+            // this will return negative angle if drifting CW
+            double angle = miniGyro.getAngle();
+
+            // TODO: correct rightX_G1 [-1.0,1.0] to adjust turn
+            // if rightX_G1 < 0 then robot will turn left
+            // if rightX_G1 > 0 then robot will turn right
+
+
+            frontLeftMotor.setPower((rightX_G1 + rightY_G1 - leftX_G1));
+            backLeftMotor.setPower((rightX_G1 + rightY_G1 + leftX_G1));
+            backRightMotor.setPower((rightX_G1 - rightY_G1 + leftX_G1));
+            frontRightMotor.setPower((rightX_G1 - rightY_G1 - leftX_G1));
+
+        }
+
+        frontLeftMotor.setPower(0.0);
+        backLeftMotor.setPower(0.0);
+        backRightMotor.setPower(0.0);
+        frontRightMotor.setPower(0.0);
+        // sets motors to zero
+    }
+
     public void goForwardNoGyro(double power, double distance){
         double rightY_G1 = 1.0 * power;
         double rightX_G1 = 0.0;
